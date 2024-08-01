2f0c22b42d1cd43a590684a0ccb6fb5815bb9ac8
Lotux00 <lotus.gtag@gmail.com>
2024-08-01 14:40:35 -0500

    Update Gnomes_Master_panel.lua

--- Gnomes_Master_panel.lua
+++ Gnomes_Master_panel.lua
@@ -1,21 +1,21 @@
 ---Making Gui ---
-MasterPanelGui = gui.get_tab("Master Panel")
+local MasterPanelGui = gui.get_tab("Master Panel")
 gui.show_message("Master Panel","is loaded")
 
 --- Strings ---
-Version = "1.0.1"
+local Version = "1.0.1"
 
 --- Credits ---
-MasterPanelGui:add_text("--- Credits ---")
-MasterPanelGui:add_separator()
-MasterPanelGui:add_text("Lotux","Buyinggnome36")
-MasterPanelGui:add_text("Script Version:" .. Version)
+local MasterPanelGui:add_text("--- Credits ---")
+local MasterPanelGui:add_separator()
+local MasterPanelGui:add_text("Lotux","Buyinggnome36")
+local MasterPanelGui:add_text("Script Version:" .. Version)
 
 
 --- Tabs ---
-TeleportTab1 = MasterPanelGui:add_tab("Teleports1")
-TeleportTab2 = MasterPanelGui:add_tab("Teleports2")
-TeleportTab3 = MasterPanelGui:add_tab("Teleports3")
+local TeleportTab1 = MasterPanelGui:add_tab("Teleports1")
+local TeleportTab2 = MasterPanelGui:add_tab("Teleports2")
+local TeleportTab3 = MasterPanelGui:add_tab("Teleports3")
 
 
 
