08605a2d46c2a5b7bfd028e524134012947616bb
Lotux00 <lotus.gtag@gmail.com>
2024-08-01 15:03:16 -0500

    Update idgaf.lua

--- idgaf.lua
+++ idgaf.lua
@@ -1,18 +1,15 @@
 ---Making gui ---
-
 TestMenuGui = gui.get_tab("Teleport Menu Version: 1.0.0")
 gui.show_message("Teleport Menu","is loaded")
------Teleports-----
 
---- GUI---
+-----Teleports-----
+--- Tabs ---
 TestMenuTab1 = TestMenuGui:add_tab("Teleports")
 TestMenuTab1:add_text("Apartments")
-
----seperator1---
 TestMenuTab1:add_separator()
 
 
------Apartment Teleports
+-----Apartment Teleports ---
 TestMenuTab1:add_button("TP to Apartment", function()
 	PED.SET_PED_COORDS_KEEP_VEHICLE(PLAYER.PLAYER_PED_ID(), -796.611, 307.099, 85.700)
 end)
